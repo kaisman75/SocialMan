ef09447248239869d29b37148139274f72c9b887
kaisman75 <kaismanai1975gmail.com>
2022-01-13 21:33:14 +0100

    bg logo

--- socialfront/src/components/login.jsx
+++ socialfront/src/components/login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import GoogleLogin from 'react-google-login'
 import FcGoogle from 'react-icons/fc'
 import { useNavigate } from 'react-router-dom'
-import shareVideo from '../components/Asets/share.mp4'
+import shareVideo from '../components/Asets/00005.mp4'
 const Login = () => {
     return (
         <div className='flex justify-start item-center flex-col h-screen'>
@@ -16,6 +16,7 @@ const Login = () => {
               autoPlay={true}
               className='w-full h-full object-cover'
               />
+              <div className='absolute flex flex-col justify-center items-center bg-blackOverlay top-0 right-0 bottom-0 left-0'></div>
           </div>
         </div>
     )
