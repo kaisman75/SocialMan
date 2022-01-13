226d9327d7b7571c88e7b67c4ee8dc7b40a23641
kais manai <kaismanai1975@gmail.com>
2022-01-13 12:32:06 +0100

    create logo page

--- socialfront/src/components/login.jsx
+++ socialfront/src/components/login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login'
-import FcGoogle from 'react-icons/fc'
+import  {FcGoogle} from 'react-icons/fc'
 import { useNavigate } from 'react-router-dom'
 import shareVideo from '../components/Asets/share.mp4'
 const Login = () => {
@@ -16,6 +16,19 @@ const Login = () => {
               autoPlay={true}
               className='w-full h-full object-cover'
               />
+          </div>
+          <div className='absolute w-full h-full flex flex-col justify-center items-center top-0 right-0 bottom-0 left-0 bg-blackOverlay'>
+            
+                  <img src="./Asets/s1.png" alt="" />
+                    <div className='mt-10'>
+                      <button 
+                       type="button"
+                       className='flex flex-row'
+                       >
+                      <FcGoogle className='mr-4'/>login with google
+                      </button>
+                      </div>
+             
           </div>
         </div>
     )
